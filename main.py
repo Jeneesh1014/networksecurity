10085026b7107e15ba87efeb1ddab49cd334eb74
Jeneesh1014 <jeneeshsurani@gmail.com>
2026-02-24 18:35:41 +0100

    Data Ingestion Created

---
+++ main.py
@@ -0,0 +1,6 @@
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+import sys
+
